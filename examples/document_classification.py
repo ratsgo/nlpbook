5904acff2665cfa85c6690aa0205ef7bd9da29b9
gichang.lee <ratsgo@naver.com>
2020-08-13 18:34:07 +0900

    [python] #1 문서 분류 튜토리얼 보완

--- examples/document_classification.py
+++ examples/document_classification.py
@@ -1,35 +1,38 @@
-from ratsnlp import *
+from ratsnlp.nlpbook import *
+from ratsnlp.nlpbook.classification import NsmcCorpus, Runner
 
 
 if __name__ == "__main__":
     args = load_arguments(json_file_path="examples/document_classification.json")
     # args = load_arguments()
-    pretrained_model_args, downstream_data_args, fine_tuning_args = args
-    set_logger(fine_tuning_args)
-    download_downstream_dataset(downstream_data_args.corpus_name,
-                                cache_dir=downstream_data_args.corpus_dir,
-                                force_download=False)
-    download_pretrained_model(pretrained_model_args.pretrained_model_name,
-                              cache_dir=pretrained_model_args.pretrained_model_cache_dir,
-                              force_download=False)
-    check_exist_checkpoints(fine_tuning_args)
-    seed_setting(fine_tuning_args.seed)
-    tokenizer = get_tokenizer(pretrained_model_args, output_dir=fine_tuning_args.output_dir)
-    corpus_class = CORPUS_FACTORY[downstream_data_args.corpus_name]()
-    # corpus_class = NsmcCorpus() # Custom Class 제작 가능
-    train_dataset, eval_dataset, test_dataset = get_datasets(corpus_class, tokenizer, args)
-    pretrained_model_config = get_pretrained_model_config(pretrained_model_args,
-                                                          task_name=downstream_data_args.task_name,
-                                                          num_labels=2,
-                                                          output_dir=fine_tuning_args.output_dir)
-    model = get_pretrained_model(pretrained_model_args.pretrained_model_cache_dir,
-                                 pretrained_model_config,
-                                 output_dir=fine_tuning_args.output_dir)
-    trainer = Trainer(
-        model=model,
-        args=fine_tuning_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        compute_metrics=build_compute_metrics_fn(downstream_data_args.task_name),
+    set_logger(args)
+    download_downstream_dataset(
+        args.downstream_corpus_name,
+        cache_dir=args.downstream_corpus_dir,
+        force_download=False
     )
-    trainer.fit()
+    download_pretrained_model(
+        args.pretrained_model_name,
+        cache_dir=args.pretrained_model_cache_dir,
+        force_download=False
+    )
+    check_exist_checkpoints(args)
+    seed_setting(args)
+    tokenizer = get_tokenizer(args)
+    corpus = NsmcCorpus()
+    train_dataloader, val_dataloader, test_dataloader = get_dataloaders(corpus, tokenizer, args)
+    model = get_pretrained_model(args, num_labels=2)
+    runner = Runner(model, args)
+    checkpoint_callback, trainer = get_trainer(args)
+    if args.do_train:
+        trainer.fit(
+            runner,
+            train_dataloader=train_dataloader,
+            val_dataloaders=val_dataloader,
+        )
+    if args.do_predict:
+        trainer.test(
+            runner,
+            test_dataloaders=test_dataloader,
+            ckpt_path=checkpoint_callback.best_model_path,
+        )
