0d6044b3c99239ce6096b84d5e5d43ad995a1f0c
gichang.lee <ratsgo@naver.com>
2020-08-15 03:09:45 +0900

    [python] #1 문서 분류 튜토리얼 보완

--- examples/document_classification.py
+++ examples/document_classification.py
@@ -3,8 +3,10 @@ from ratsnlp.nlpbook.classification import NsmcCorpus, Runner
 
 
 if __name__ == "__main__":
-    args = load_arguments(json_file_path="examples/document_classification.json")
-    # args = load_arguments()
+    # 원하는 arguments로 변경 가능
+    # dataclass만 정의하면 됨
+    args = load_arguments(Arguments, json_file_path="examples/document_classification.json")
+    # args = load_arguments(Arguments)
     set_logger(args)
     download_downstream_dataset(
         args.downstream_corpus_name,
@@ -18,10 +20,21 @@ if __name__ == "__main__":
     )
     check_exist_checkpoints(args)
     seed_setting(args)
+    # huggingface PretrainedTokenizer이기만 하면 됨
+    # 원하는 토크나이저로 교체해 사용 가능
     tokenizer = get_tokenizer(args)
+    # from ratsnlp.nlpbook.dataset import Corpus
+    # 위의 클래스를 상속받아 커스텀하게 사용 가능
     corpus = NsmcCorpus()
-    train_dataloader, val_dataloader, test_dataloader = get_dataloaders(corpus, tokenizer, args)
-    model = get_pretrained_model(args, num_labels=2)
+    # text > input_ids, attention_mask, token_type_ids 형태 말고 추가, 삭제, 변경하려면...
+    # Dataset 정의 : 핵심은 init에 전체 데이터셋을 ids로 변환해 불러오고, getitem으로 인스턴스 하나씩만 넘겨주면 됨
+    # Dataset, collate_fn만 사용자 정의하면 됨
+    # 이제 나머지는 datasampler 등과 엮어서 DataLoader로 묶어주기
+    train_dataloader, val_dataloader, test_dataloader = get_dataloaders(args, tokenizer, corpus)
+    # huggingface PretrainedModel이기만 하면 됨
+    # 원하는 모델로 교체해 사용 가능, 단 체크포인트가 사전에 읽혀져야 한다(torch.load)
+    model = get_pretrained_model(args, num_labels=corpus.num_labels)
+    # 파이토치 라이트닝 모듈 상속받아 커스텀하게 사용 가능
     runner = Runner(model, args)
     checkpoint_callback, trainer = get_trainer(args)
     if args.do_train:
