57c24993d5282fbfb10b8590f537faee82b0ba40
ratsgo <ratsgo@naver.com>
2021-03-28 19:40:45 +0900

    [Python] #6 문장 생성 모델 파인튜닝 예제 보완

--- examples/sentence_generation/train_local.py
+++ examples/sentence_generation/train_local.py
@@ -1,10 +1,8 @@
 import sys
 from ratsnlp import nlpbook
 from Korpora import Korpora
-from transformers import GPT2LMHeadModel
-from ratsnlp.nlpbook import load_arguments
 from ratsnlp.nlpbook.generation import *
-from ratsnlp.nlpbook.tokenizers import KoGPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
 
 
@@ -12,31 +10,33 @@ if __name__ == "__main__":
     # case1 : python train_local.py
     if len(sys.argv) == 1:
         args = GenerationTrainArguments(
-            pretrained_model_name="taeminlee/kogpt2",
+            pretrained_model_name="kogpt2",
+            pretrained_model_cache_dir="model",
             downstream_corpus_root_dir="data",
             downstream_corpus_name="nsmc",
             force_download=True,
             downstream_model_dir="checkpoint/sentence-generation",
             do_eval=True,
-            batch_size=96,
+            batch_size=32,
             max_seq_length=32,
             epochs=10,
         )
     # case2 : python train_local.py train_config.json
     elif len(sys.argv) == 2 and sys.argv[-1].endswith(".json"):
-        args = load_arguments(GenerationTrainArguments, json_file_path=sys.argv[-1])
+        args = nlpbook.load_arguments(GenerationTrainArguments, json_file_path=sys.argv[-1])
     # case3 : python train_local.py --pretrained_model_name beomi/kcbert-base --downstream_corpus_root_dir data --downstream_corpus_name nsmc --downstream_task_name document-classification --downstream_model_dir checkpoint/document-classification --do_eval --batch_size 32
     else:
-        args = load_arguments(GenerationTrainArguments)
+        args = nlpbook.load_arguments(GenerationTrainArguments)
     nlpbook.set_logger(args)
+    nlpbook.download_pretrained_model(args)
     Korpora.fetch(
         corpus_name=args.downstream_corpus_name,
         root_dir=args.downstream_corpus_root_dir,
         force_download=args.force_download,
     )
     nlpbook.seed_setting(args)
-    tokenizer = KoGPT2Tokenizer.from_pretrained(
-        args.pretrained_model_name,
+    tokenizer = GPT2Tokenizer.from_pretrained(
+        args.pretrained_model_cache_dir
     )
     corpus = NsmcCorpus()
     train_dataset = GenerationDataset(
