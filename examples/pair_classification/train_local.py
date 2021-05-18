bbf979fbb50f859a91b68c2e13b43c165593858b
gichang.lee <ratsgo@naver.com>
2021-05-19 00:30:56 +0900

    [Ipynb] paircls 튜토리얼 보완

--- examples/pair_classification/train_local.py
+++ examples/pair_classification/train_local.py
@@ -1,11 +1,10 @@
 import sys
 from ratsnlp import nlpbook
-from Korpora import Korpora
 from ratsnlp.nlpbook import load_arguments
 from torch.utils.data import DataLoader, SequentialSampler, RandomSampler
 from transformers import BertConfig, BertTokenizer, BertForSequenceClassification, set_seed
 from ratsnlp.nlpbook.classification import ClassificationTrainArguments, ClassificationDataset, ClassificationTask
-from ratsnlp.nlpbook.paircls import KorNLICorpus
+from ratsnlp.nlpbook.paircls import KlueNLICorpus
 
 
 if __name__ == "__main__":
@@ -15,12 +14,12 @@ if __name__ == "__main__":
             pretrained_model_name="beomi/kcbert-base",
             downstream_task_name="pair-classification",
             downstream_corpus_root_dir="data",
-            downstream_corpus_name="kornli",
+            downstream_corpus_name="klue-nli",
             force_download=True,
             downstream_model_dir="checkpoint/pair-classification",
-            batch_size=256,
+            batch_size=32,
             max_seq_length=64,
-            epochs=4,
+            epochs=5,
         )
     # case2 : python train_local.py train_config.json
     elif len(sys.argv) == 2 and sys.argv[-1].endswith(".json"):
@@ -29,17 +28,13 @@ if __name__ == "__main__":
     else:
         args = load_arguments(ClassificationTrainArguments)
     nlpbook.set_logger(args)
-    Korpora.fetch(
-        corpus_name=args.downstream_corpus_name,
-        root_dir=args.downstream_corpus_root_dir,
-        force_download=args.force_download,
-    )
+    nlpbook.download_downstream_dataset(args)
     set_seed(args.seed)
     tokenizer = BertTokenizer.from_pretrained(
         args.pretrained_model_name,
         do_lower_case=False,
     )
-    corpus = KorNLICorpus()
+    corpus = KlueNLICorpus()
     train_dataset = ClassificationDataset(
         args=args,
         corpus=corpus,
